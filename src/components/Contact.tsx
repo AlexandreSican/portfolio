727e131a96da24066d51b48a02eec4075800c50d
Alexandre Sicchieri Cancian <105050121+AlexandreSican@users.noreply.github.com>
2022-06-27 19:18:56 -0300

    Update
    Update of Contact and Portfolio.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -32,7 +32,7 @@ export const Contact = () => {
   }
 
   return (
-    <div className="bg-gray-900 bg-cover bg-no-repeat flex flex-col w-full h-[100vh]">
+    <div className="bg-gray-900 bg-cover bg-no-repeat flex flex-col w-full h-full min-h-[100vh]">
       <div className="w-full h-full bg-contact bg-no-repeat bg-auto bg-top flex flex-col items-center">
         <h1 className="text-5xl text-white uppercase flex justify-center pt-[9.6375rem]">contact</h1>
         <div className="flex justify-center items-center w-[70%] h-[60%] m-20">
@@ -68,7 +68,7 @@ export const Contact = () => {
             <button
               type="submit"
               disabled={loading}
-              className="w-[82%] border-2 rounded-xl text-5xl uppercase mt-6 bg-black text-white p-8 hover:bg-white hover:text-black transition-colors disabled:opacity-50"
+              className="w-[82%] border-2 rounded-xl text-5xl uppercase mt-6 bg-black text-white p-2 max:p-8 hover:bg-white hover:text-black transition-colors disabled:opacity-50"
             >Send</button>
           </form>
         </div>
--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -2,10 +2,10 @@ import { LinesPort } from "./svgs/LinesPort"
 
 export const Portfolio = () => {
   return (
-    <div className="w-full h-[auto] max:h-[100vh] bg-no-repeat flex flex-col bg-auto bg-gray-300 bg-center items-center pt-[2.5rem] max:pt-[4.525rem] ultra:bg-none ultra:pt-32">
+    <div className="w-full h-[full] max:min-h-[100vh] bg-no-repeat flex flex-col bg-auto bg-gray-300 bg-center items-center pt-[2.5rem] max:pt-[4.525rem] ultra:bg-none ultra:pt-32">
       <h1 className="uppercase justify-center items-center text-[3rem] ">portfolio</h1>
       <div className="flex justify-center mt-44">
-      <div className="grid  grid-cols-1 grid-rows-3 max:grid-cols-3 max:grid-rows-1 w-full gap-24">
+      <div className="grid  grid-cols-1 grid-rows-3 max:grid-cols-3 max:grid-rows-1 w-full gap-24 mb-4">
         <div className="drop-shadow-xl rounded-3xl flex flex-col w-[22.8125rem] h-[34.1875rem] bg-[#0A0A0A]">
           <img src="./src/assets/pizza.png" alt="" />
           <div className="justify-left flex flex-col items-left opacity-100">
