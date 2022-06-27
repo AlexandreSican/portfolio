21df39c3f690f62cc6a4537cc9660f3f46cdddf6
Alexandre Sicchieri Cancian <105050121+AlexandreSican@users.noreply.github.com>
2022-06-27 19:22:47 -0300

    Update portfolio
    Update code to import imgs with URL

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -7,21 +7,21 @@ export const Portfolio = () => {
       <div className="flex justify-center mt-44">
       <div className="grid  grid-cols-1 grid-rows-3 max:grid-cols-3 max:grid-rows-1 w-full gap-24 mb-4">
         <div className="drop-shadow-xl rounded-3xl flex flex-col w-[22.8125rem] h-[34.1875rem] bg-[#0A0A0A]">
-          <img src="./src/assets/pizza.png" alt="" />
+          <img src="https://github.com/AlexandreSican/portfolio/blob/main/src/assets/pizza.png?raw=true" alt="" />
           <div className="justify-left flex flex-col items-left opacity-100">
             <h1 className="text-white text-3xl p-4">Pizzaria</h1>
             <p className="text-white text-xl p-2">Projeto realizado pra tangibilizar meus conhecimentos</p>
           </div>
         </div>
         <div className="drop-shadow-xl rounded-3xl flex flex-col w-[22.8125rem] h-[34.1875rem] bg-[#0A0A0A]">
-          <img src="./src/assets/batery.png" alt="" />
+          <img src="https://github.com/AlexandreSican/portfolio/blob/main/src/assets/batery.png?raw=true" alt="" />
           <div className="justify-left flex flex-col items-left opacity-100">
             <h1 className="text-white text-3xl p-4">Bateria</h1>
             <p className="text-white text-xl p-2">Projeto realizado pra tangibilizar meus conhecimentos</p>
           </div>
         </div>
         <div className="drop-shadow-xl rounded-3xl flex flex-col w-[22.8125rem] h-[34.1875rem] bg-[#0A0A0A]">
-          <img src="./src/assets/weather.png" alt="" />
+          <img src="https://github.com/AlexandreSican/portfolio/blob/main/src/assets/weather.png?raw=true" alt="" />
           <div className="justify-left flex flex-col items-left opacity-100">
             <h1 className="text-white text-3xl p-4 ">Clima Tempo</h1>
             <p className="text-white text-xl p-2">Projeto realizado pra tangibilizar meus conhecimentos</p>
