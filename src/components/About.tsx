4934b796bc9e9ca58d1e95a3b1d7a55db579676b
Alexandre Sicchieri Cancian <105050121+AlexandreSican@users.noreply.github.com>
2022-06-27 19:36:56 -0300

    Update About
    Update about with a conditional that verify the width of screen and set the width of Icons

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,6 +2,7 @@ import { Icons } from "./svgs/Icons"
 
 import { LinesAbout } from "./svgs/LinesAbout"
 
+const width = window.screen.width;
 export const About = () => {
   return (
     <div className="w-full min-h-[100vh] bg-gray-900 relative">
@@ -16,21 +17,26 @@ export const About = () => {
         </div>
         <div className="w-[100vw] max:w-[40vw] flex flex-col  justify-start  items-center mt-24">
           <div className="w-[60%] max:w-full flex items-center justify-center flex-col">
-          <h1 className="text-gray-100 text-[3rem]"><span className="text-blue-400">Hello</span>, Welcome to my Portfolio</h1>
-          <p className="text-gray-100 text-[2.25rem]">My name is
-            <span className="text-blue-400"> Alexandre Sicchieri Cancian</span>
-            , i'm 27 years old and i'm specializing in <span className="text-blue-400">front web development</span>
-            , currently studying <span className="text-blue-400">React</span> and its libraries.</p>
+            <h1 className="text-gray-100 text-[3rem]"><span className="text-blue-400">Hello</span>, Welcome to my Portfolio</h1>
+            <p className="text-gray-100 text-[2.25rem]">My name is
+              <span className="text-blue-400"> Alexandre Sicchieri Cancian</span>
+              , i'm 27 years old and i'm specializing in <span className="text-blue-400">front web development</span>
+              , currently studying <span className="text-blue-400">React</span> and its libraries.</p>
           </div>
-          <div className="mt-20 mb-20">
-            <Icons />
-          </div>
+          {width >= 1500 ?
+            (<div className="mt-20 mb-20 max:">
+              <Icons width={"600px"} />
+            </div>)
+            :
+            (<div className="mt-20 mb-20 max:">
+              <Icons width={"350px"} />
+            </div>)}
         </div>
       </div>
-        <div className="w-[50%] absolute right-52 bottom-0 max:flex hidden">
+      <div className="w-[50%] absolute right-52 bottom-0 max:flex hidden">
         <LinesAbout heigth="165px" />
       </div>
-      
+
     </div>
   )
 }
