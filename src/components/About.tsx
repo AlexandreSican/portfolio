e4d6a9d516a8904b879f13d68335fab709991734
Alexandre Sicchieri Cancian <105050121+AlexandreSican@users.noreply.github.com>
2022-06-27 20:03:16 -0300

    Update About
    Update file about, with responsivity icons. (setState and listenner on width screen)

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,16 @@
+import { useState } from "react"
 import { Icons } from "./svgs/Icons"
 
-import { LinesAbout } from "./svgs/LinesAbout"
-
-const width = window.screen.width;
 export const About = () => {
+
+  const [width, setWidth] = useState('')
+  window.addEventListener('resize', () => {
+    const widthScreen = window.screen.width.toString()
+    setWidth(widthScreen)
+  })
+  
   return (
-    <div className="w-full min-h-[100vh] bg-gray-900 relative">
+    <div className="w-full h-full min-h-[100vh] bg-gray-900 relative">
       <h1 className="uppercase flex flex-1 justify-center pt-32 text-[3rem] text-gray-100">about</h1>
       <div className="block max:flex">
         <div className="w-[100%] max:w-[50vw] flex items-center justify-center mt-28">
@@ -23,19 +28,18 @@ export const About = () => {
               , i'm 27 years old and i'm specializing in <span className="text-blue-400">front web development</span>
               , currently studying <span className="text-blue-400">React</span> and its libraries.</p>
           </div>
-          {width >= 1500 ?
-            (<div className="mt-20 mb-20 max:">
-              <Icons width={"600px"} />
-            </div>)
-            :
-            (<div className="mt-20 mb-20 max:">
-              <Icons width={"350px"} />
-            </div>)}
+           {parseInt(width) >= 1500 ? (
+            <div className="mt-20 mb-20">
+            <Icons width={"600px"} />
+          </div>
+           ):(
+            <div className="mt-20 mb-20">
+            <Icons width={"350px"} />
+          </div>
+           )}
         </div>
       </div>
-      <div className="w-[50%] absolute right-52 bottom-0 max:flex hidden">
-        <LinesAbout heigth="165px" />
-      </div>
+      
 
     </div>
   )
