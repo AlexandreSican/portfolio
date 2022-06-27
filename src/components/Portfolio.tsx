1d37136727264c691c23ac619c7409f0e4e36928
Alexandre Sicchieri Cancian <105050121+AlexandreSican@users.noreply.github.com>
2022-06-27 19:28:29 -0300

    Update Portfolio
    Update archor of imgs and their descriptions

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -7,24 +7,24 @@ export const Portfolio = () => {
       <div className="flex justify-center mt-44">
       <div className="grid  grid-cols-1 grid-rows-3 max:grid-cols-3 max:grid-rows-1 w-full gap-24 mb-4">
         <div className="drop-shadow-xl rounded-3xl flex flex-col w-[22.8125rem] h-[34.1875rem] bg-[#0A0A0A]">
-          <img src="https://github.com/AlexandreSican/portfolio/blob/main/src/assets/pizza.png?raw=true" alt="" />
+          <a href="https://alexandresican.github.io/compra-pizza/" target="_blank"><img src="https://github.com/AlexandreSican/portfolio/blob/main/src/assets/pizza.png?raw=true" alt="" /></a>
           <div className="justify-left flex flex-col items-left opacity-100">
-            <h1 className="text-white text-3xl p-4">Pizzaria</h1>
-            <p className="text-white text-xl p-2">Projeto realizado pra tangibilizar meus conhecimentos</p>
+            <h1 className="text-white text-3xl p-4">Pizzeria</h1>
+            <p className="text-white text-xl p-2">Project created to make my knowledge in JavaScript, HTML and CSS tangiblet</p>
           </div>
         </div>
         <div className="drop-shadow-xl rounded-3xl flex flex-col w-[22.8125rem] h-[34.1875rem] bg-[#0A0A0A]">
-          <img src="https://github.com/AlexandreSican/portfolio/blob/main/src/assets/batery.png?raw=true" alt="" />
+         <a href="https://alexandresican.github.io/projeto-bateria/" target="_blank"> <img src="https://github.com/AlexandreSican/portfolio/blob/main/src/assets/batery.png?raw=true" alt="" /></a>
           <div className="justify-left flex flex-col items-left opacity-100">
-            <h1 className="text-white text-3xl p-4">Bateria</h1>
-            <p className="text-white text-xl p-2">Projeto realizado pra tangibilizar meus conhecimentos</p>
+            <h1 className="text-white text-3xl p-4">Drums</h1>
+            <p className="text-white text-xl p-2">Project created to make my knowledge in JavaScript, HTML and CSS tangible</p>
           </div>
         </div>
         <div className="drop-shadow-xl rounded-3xl flex flex-col w-[22.8125rem] h-[34.1875rem] bg-[#0A0A0A]">
-          <img src="https://github.com/AlexandreSican/portfolio/blob/main/src/assets/weather.png?raw=true" alt="" />
+         <a href="https://alexandresican.github.io/climatempo/" target="_blank"> <img src="https://github.com/AlexandreSican/portfolio/blob/main/src/assets/weather.png?raw=true" alt="" /></a>
           <div className="justify-left flex flex-col items-left opacity-100">
-            <h1 className="text-white text-3xl p-4 ">Clima Tempo</h1>
-            <p className="text-white text-xl p-2">Projeto realizado pra tangibilizar meus conhecimentos</p>
+            <h1 className="text-white text-3xl p-4 ">Weather</h1>
+            <p className="text-white text-xl p-2">Project created to make my knowledge in JavaScript, HTML and CSS tangible</p>
           </div>
         </div>
 
